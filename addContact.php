59750b5aa0dc766e874f12c076ed47f13667c0ff
Zach <zachary.rudzinski@gmail.com>
2017-04-17 10:48:42 -0500

    Added client side validation for addContact.php

--- addContact.php
+++ addContact.php
@@ -8,7 +8,8 @@ require_once('sql/dbQueries.php');
 function printAddContactForm() {
 	echo "<div class='formWrapper'>";
 	echo "<h3>Add a contact</h3>";
-	echo '<form action="addContact.php" method="POST">';
+	echo '<form action="addContact.php" method="POST" ';
+	echo 'name="addContact" onsubmit="return validateAddContact()">';
 		echo '<label>First name</label>';
 		echo '<input name="firstName" />';
 
@@ -86,10 +87,77 @@ function createContact() {
 <head>
 	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	<title>voipSMS</title>
+	
+<script>
+function validateDID(did) {
+	if(/\d{10}/.test(did))
+		return (true)
+	
+	return (false)
+}
+
+// Make sure form is filled completely and such.
+function validateAddContact() {
+	var errors = [];
+	var form = document.forms['addContact'];
+	var errorMessage = document.getElementById('formErrorMessage');
+	
+	// Clear error classes from inputs
+	form['firstName'].classList.remove("formError");
+	form['lastName'].classList.remove("formError");
+	form['did'].classList.remove("formError");
+	errorMessage.classList.remove('error');
+	
+	// Clear the error div
+	errorMessage.innerHTML = "";
+	
+	// -- Begin processing form --
+	// Making sure values aren't empty
+	if(form['firstName'].value == "") {
+		errors.push("First name cannot be empty.");
+		form['firstName'].classList.add('formError');
+	}
+	
+	if(form['lastName'].value == "") {
+		errors.push("Last name cannot be empty.");
+		form['lastName'].classList.add('formError');
+	}
+	
+	if(form['did'].value == "") {
+		errors.push("Contact phone number cannot be empty.");
+		form['did'].classList.add('formError');
+	}
+
+	// Making sure the contact DID is in the right format (dddddddddd)
+	if(!validateDID(form['did'].value)) {
+		errors.push("Contact phone number is not in the right format. Example format: '1231231234'");
+		form['did'].classList.add('formError');
+	}
+
+	// -- Writing errors --                                                
+	var numErrors = errors.length;                                         
+	if(numErrors > 0) {                                                    
+		// Loop though errors and write them to the error message div      
+		errorMessage.innerHTML = "Errors found while processing the form:";
+		
+		for(var i = 0; i < numErrors; i++) {                               
+			errorMessage.innerHTML += "<br />";                            
+			errorMessage.innerHTML += errors[i];                           
+		}                                                                  
+		
+		errorMessage.classList.add('error');                               
+		return false;                                                      
+	}                                                                      
+	
+	return true;                                                           
+}
+
+</script>
 </head>
 <body>
 <?php 
 	include_once("header.php");
+	echo "<div id='formErrorMessage'></div>";
 
 	// Make sure the user is actually logged in
 	if(!isset($_SESSION['auth'])) {
