cfef1f58a188e8359d89588b42dd11b6c0db79ca
Zach <xwxrequestxwx@gmail.com>
2018-05-24 17:25:41 -0400

    Begun revamping CSS: Header, and login form
    - Moved login form from header to the login page
    - Revamped CSS for the header and login form

--- login.php
+++ login.php
@@ -6,17 +6,41 @@
 	Also, display $message to them.
 */
 function printLoginPage($message) {
-	echo ' <!DOCTYPE html>
+	echo '<!DOCTYPE html>
 		<html> 
 		<head> 
 		    <meta charset="utf-8">
-			<link rel="stylesheet" type="text/css" href="css/main.css" /> 
-			<title>voipSMS: Login failed</title> 
+			<title>voipSMS: Login</title> 
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		</head> 
 		<body>';
-		require_once('header.php'); 
-	echo "<div class='error'>{$message}</div>";
+	include_once('header.php'); 
+
+	// Login form
+	echo '
+	<form action="login.php" method="POST">
+		<div class="form-group">
+			<label for="emailInput">Email Address</lable>
+			<input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" name="vms_email" />
+		</div>
+	
+		<div class="form-group">
+			<label for="passwordInput">Password</lable>
+			<input type="password" class="form-control" id="passwordInput" aria-describedby="passwordHelp" placeholder="Password" name="userPassword" />
+		</div>
+
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form> ';
+
+	// Errors, if any
+	if($message !== "") {
+		echo "<div class='error'>{$message}</div>";
+	}
+
 	echo '</body> 
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		</html>';
 }
 
@@ -62,8 +86,9 @@ function loginUser() {
 	Else:	
 		Show them an error message.
 */
-if($_SERVER['REQUEST_METHOD'] != "POST") {
-	printLoginPage("Error: No login information supplied.");
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+	// User got here via GET. Display the login form.
+	printLoginPage();
 } else {
 	// Also, test if they supplied the right login info
 	if(isset($_POST['vms_email'])
